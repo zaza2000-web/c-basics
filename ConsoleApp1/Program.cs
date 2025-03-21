df48495dae1d483c53d626ddf33537547919495a
Zaza <zazuka2000badzagua@gmail.com>
2025-03-21 13:06:27 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string NameValue = "EUR";
+            double EurCost = 12.23;
+            string Pay = Console.ReadLine();
+            double parsedSum = double.Parse(Pay);
+            double resultEuro = parsedSum / EurCost;
+            Console.WriteLine("You pay: " + Pay + " And u get: " + resultEuro + NameValue);
+
+
+        }
+    }
+}
